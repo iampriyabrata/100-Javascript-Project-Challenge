44535c7cc533bcad389c9961abb769d36611a3db
Priyabrata <priyabrataxofficial@gmail.com>
2023-05-29 10:16:14 +0530

    js file inserted

---
+++ Change Background Color Project/script.js
@@ -0,0 +1,26 @@
+const buttonEl = document.getElementById("change-Bg-Btn");
+
+const colorArray = [rose, peach, beige, icterine, vanilla, malachite, mint, persianGreen, robinEggBlue, turquise, processCyan, babyBlue, comboliaBlue, tuftsBlue, africanViolet, wisteria, salmonPink]
+
+//colors with color hexcode...
+const rose = "#FF0080";
+const peach = "#FFE5B4";
+const beige = "#F5F5DC";
+const icterine = "#FCF75E";
+const vanilla = "#F3E5AB";
+const malachite = "#0BDA51";
+const mint = "#3EB489";
+const persianGreen = "#00A693";
+const robinEggBlue = "#00CCCC";
+const turquise = "#40E0D0";
+const processCyan = "#00B7EB";
+const babyBlue = "#89CFF0";
+const comboliaBlue = "#B9D9EB";
+const tuftsBlue = "#3E8EDE";
+const africanViolet = "#B284BE";
+const wisteria = "#C9A0DC";
+const salmonPink = "#FF91A4";
+
+buttonEl.addEventListener("click", function(){
+    alert("clicked!")
+})
